d7b779394122d622ccf717e58682d9a014c9ec8f
erfanjalinoosi <erfanjalinoosi@gmail.com>
2023-02-13 01:16:16 +0330

    erf

--- middleware/index.js
+++ middleware/index.js
@@ -4,27 +4,34 @@ const User = require("../models/user");
 
 middlewareObject.isLoggedIn = async (req, res, next) => {
 
-  if (!req.header('authorization')) {
+  try{
+    if (!req.header('authorization')) {
+      return res.status(400).send({
+        message: "User should login."
+      })
+    }
+  
+    const token = req.header('authorization').split(' ');
+  
+    const decrypted = jwt.decode(token[1]);
+    console.log(decrypted);
+    const user = await User.findById(decrypted._id);
+  
+    if (!user) {
+      return res.status(400).send({
+        message: "User should login."
+      })
+    }
+  
+    req.user = user
+  
+    next()
+  }catch(err){
     return res.status(400).send({
-      message: "User should login."
+      message: err.message
     })
   }
-
-  const token = req.header('authorization').split(' ');
-
-  const decrypted = jwt.decode(token[1]);
-
-  const user = await User.findById(decrypted._id);
-
-  if (!user) {
-    return res.status(400).send({
-      message: "User should login."
-    })
-  }
-
-  req.user = user
-
-  next()
+  
 };
 
 module.exports = middlewareObject;
